e438a2de73b88ef241fcc13e88a8066ffc508d06
Christof Vollrath <christof@taobits.net>
2020-05-09 00:26:02 +0200

    Change the Text

--- DiceRolling/app/src/main/java/net/taobits/dicerolling/MainActivity.kt
+++ DiceRolling/app/src/main/java/net/taobits/dicerolling/MainActivity.kt
@@ -1,18 +1,30 @@
 package net.taobits.dicerolling
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var rollButton: Button
+    lateinit var resultText: TextView
+    val randomGenerator = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton = findViewById<Button>(R.id.roll_button)
+        rollButton = findViewById<Button>(R.id.roll_button)
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button click", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+        resultText = findViewById<TextView>(R.id.result_text)
+
+    }
+
+    private fun rollDice() {
+        val randomInt = randomGenerator.nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
