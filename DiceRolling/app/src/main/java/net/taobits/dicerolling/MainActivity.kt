a4ada7a1ace05bb14826bdb5d1074c5e34d8c1df
Christof Vollrath <christof@taobits.net>
2020-05-09 00:46:57 +0200

    Adding Image Resources and ImageView

--- DiceRolling/app/src/main/java/net/taobits/dicerolling/MainActivity.kt
+++ DiceRolling/app/src/main/java/net/taobits/dicerolling/MainActivity.kt
@@ -3,28 +3,33 @@ package net.taobits.dicerolling
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var rollButton: Button
-    lateinit var resultText: TextView
-    val randomGenerator = Random()
+    private val rollButton by lazy { findViewById<Button>(R.id.roll_button)!! }
+    private val diceImage by lazy { findViewById<ImageView>(R.id.dice_image)!! }
+    private val randomGenerator = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        rollButton = findViewById<Button>(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
         }
-        resultText = findViewById<TextView>(R.id.result_text)
-
     }
 
     private fun rollDice() {
-        val randomInt = randomGenerator.nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomGenerator.nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6 // only 6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
