e59586f5ed9bd4a6eb28eee7e0c9cd446b531b93
Christof Vollrath <christof@taobits.net>
2020-05-08 17:55:53 +0200

    findViewById

--- DiceRolling/app/src/main/java/net/taobits/dicerolling/MainActivity.kt
+++ DiceRolling/app/src/main/java/net/taobits/dicerolling/MainActivity.kt
@@ -2,11 +2,14 @@ package net.taobits.dicerolling
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton = findViewById<Button>(R.id.roll_button)
+        rollButton.text = "Let's roll"
     }
 }
